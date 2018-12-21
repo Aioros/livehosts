a86279088fa823f5caf0d7cab37bd8dba39307ae
Aioros <aioros83@gmail.com>
2018-12-21 15:24:46 +0100

    Fixed a couple of serious bugs

--- background.js
+++ background.js
@@ -37,101 +37,103 @@ var hostData = {};
 var storageKey = "livehosts";
 
 chrome.storage.sync.get(storageKey, function(data) {
-    if (data[storageKey]) {
-        hostData = data[storageKey];
+    
+    if (!data[storageKey])
+        data[storageKey] = [];
 
-        var hostMatches = hostData.map(host => "*://" + host.hostName + "/*");
-        var ipMatches = hostData.reduce((acc, cur) => [...acc, ...cur.ips.map(el => "*://" + el.ip + "/*")], []);
+    hostData = data[storageKey];
 
-        var requests = {};
+    var hostMatches = hostData.map(host => "*://" + host.hostName + "/*");
+    var ipMatches = hostData.reduce((acc, cur) => [...acc, ...cur.ips.map(el => "*://" + el.ip + "/*")], []);
 
-        chrome.webRequest.onBeforeRequest.addListener(function(details) {//console.log("request", details);
-            var parser = parseUrl(details.url);
-            
-            let hostMatch = hostData.find(host => host.hostName === parser.hostname);
-            if (hostMatch) {    // we have a request for one of the hosts in the hosts file
-                let ruleActive = hostMatch.ips.find(rule => rule.active != rule.exceptions.includes(details.tabId));
-                // i.e. the rule is active in this tab if it's active and the tab is not an exception
-                // or if it's inactive and the tab is an exception
-                if (ruleActive) {
-                    // Add the host/IP/tab data to our record
-                    if (details.type === "main_frame") {
-                        requests[details.tabId] = {
-                            hostname: parser.hostname,
-                            ip: ruleActive.ip,
-                            tabId: details.tabId,
-                            requestId: details.requestId
-                        };
-                    }
-                    // redirect to the IP, we will add the Host header in the onBeforeSendHeaders listener
-                    return {
-                        redirectUrl: parser.protocol + "//" + ruleActive.ip + (parser.port != "" ? ":" + parser.port : "") +
-                                        parser.pathname + parser.search + parser.hash
-                    };
-                }
-            } else {    // we have a request for one of the IPs in the hosts file
-                // case 1: e.g. 127.0.0.1/www.example.com(/.*)?
-                // look for a matching IP/hostname pair in our hosts file
-                let host = hostData.find(host => parser.pathname.startsWith("/"+host.hostName) && host.ips.find(rule => rule.ip === parser.hostname));
-                if (host) {
-                    // update the record
+    var requests = {};
+
+    chrome.webRequest.onBeforeRequest.addListener(function(details) {//console.log("request", details);
+        var parser = parseUrl(details.url);
+        
+        let hostMatch = hostData && hostData.find(host => host.hostName === parser.hostname);
+        if (hostMatch) {    // we have a request for one of the hosts in the hosts file
+            let ruleActive = hostMatch.ips.find(rule => rule.active != (rule.exceptions && rule.exceptions.includes(details.tabId)));
+            // i.e. the rule is active in this tab if it's active and the tab is not an exception
+            // or if it's inactive and the tab is an exception
+            if (ruleActive) {
+                // Add the host/IP/tab data to our record
+                if (details.type === "main_frame") {
                     requests[details.tabId] = {
-                        hostname: host.hostName,
-                        ip: parser.hostname,
+                        hostname: parser.hostname,
+                        ip: ruleActive.ip,
                         tabId: details.tabId,
                         requestId: details.requestId
                     };
-                    // redirect to the IP, removing the hostname from the path
-                    // (we will add the Host header in the onBeforeSendHeaders listener)
-                    return {
-                        redirectUrl: parser.protocol + "//" + parser.hostname + (parser.port != "" ? ":" + parser.port : "") +
-                                        parser.pathname.replace("/" + host.hostName, "/").replace("//", "/") + parser.search + parser.hash
-                    }
                 }
-                // case 2: e.g. 127.0.0.1(/.*)?
-                // no redirect, we will manage the Host header (if needed) in the onBeforeSendHeaders listener
-                return;
+                // redirect to the IP, we will add the Host header in the onBeforeSendHeaders listener
+                return {
+                    redirectUrl: parser.protocol + "//" + ruleActive.ip + (parser.port != "" ? ":" + parser.port : "") +
+                                    parser.pathname + parser.search + parser.hash
+                };
             }
-        }, {
-            urls: hostMatches.concat(ipMatches) // we listen to both hostnames and IPs in our hosts file
-        }, ["blocking"]);
+        } else {    // we have a request for one of the IPs in the hosts file
+            // case 1: e.g. 127.0.0.1/www.example.com(/.*)?
+            // look for a matching IP/hostname pair in our hosts file
+            let host = hostData.find(host => parser.pathname.startsWith("/"+host.hostName) && host.ips.find(rule => rule.ip === parser.hostname));
+            if (host) {
+                // update the record
+                requests[details.tabId] = {
+                    hostname: host.hostName,
+                    ip: parser.hostname,
+                    tabId: details.tabId,
+                    requestId: details.requestId
+                };
+                // redirect to the IP, removing the hostname from the path
+                // (we will add the Host header in the onBeforeSendHeaders listener)
+                return {
+                    redirectUrl: parser.protocol + "//" + parser.hostname + (parser.port != "" ? ":" + parser.port : "") +
+                                    parser.pathname.replace("/" + host.hostName, "/").replace("//", "/") + parser.search + parser.hash
+                }
+            }
+            // case 2: e.g. 127.0.0.1(/.*)?
+            // no redirect, we will manage the Host header (if needed) in the onBeforeSendHeaders listener
+            return;
+        }
+    }, {
+        urls: hostMatches.concat(ipMatches) // we listen to both hostnames and IPs in our hosts file
+    }, ["blocking"]);
 
-        chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
-            if (requests[details.tabId]) {  // it's one of "those" requests
-                // add the appropriate Host header
-                //console.log("adding host " + requests[details.tabId].hostname);
-                details.requestHeaders.push({
-                    name: "Host",
-                    value: requests[details.tabId].hostname
+    chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+        if (requests[details.tabId]) {  // it's one of "those" requests
+            // add the appropriate Host header
+            //console.log("adding host " + requests[details.tabId].hostname);
+            details.requestHeaders.push({
+                name: "Host",
+                value: requests[details.tabId].hostname
+            });
+            return {requestHeaders: details.requestHeaders};
+        }
+    }, {
+        urls: hostMatches.concat(ipMatches) // we listen to both hostnames and IPs in our hosts file
+    }, ["requestHeaders", "blocking"]);
+
+    // This is the ugly trick. We can't actually replace the URL in the address bar completely without
+    // a redirect, so we settle for a not-so-subtle fallback: we add the hostname right after the IP.
+    // We send the hostname as a message to the tab, and use the History API in its content script.
+    chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
+        if (info.status === "complete") {
+            if (requests[tabId]) {
+                var host = requests[tabId].hostname;
+                chrome.tabs.sendMessage(tabId, {host: host}, function (response) {
+                    if (chrome.runtime.lastError)
+                        console.log(chrome.runtime.lastError);
                 });
-                return {requestHeaders: details.requestHeaders};
+                // delete the completed request from our record
+                delete requests[tabId];
             }
-        }, {
-            urls: hostMatches.concat(ipMatches) // we listen to both hostnames and IPs in our hosts file
-        }, ["requestHeaders", "blocking"]);
+        }
+    });
 
-        // This is the ugly trick. We can't actually replace the URL in the address bar completely without
-        // a redirect, so we settle for a not-so-subtle fallback: we add the hostname right after the IP.
-        // We send the hostname as a message to the tab, and use the History API in its content script.
-        chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
-            if (info.status === "complete") {
-                if (requests[tabId]) {
-                    var host = requests[tabId].hostname;
-                    chrome.tabs.sendMessage(tabId, {host: host}, function (response) {
-                        if (chrome.runtime.lastError)
-                            console.log(chrome.runtime.lastError);
-                    });
-                    // delete the completed request from our record
-                    delete requests[tabId];
-                }
-            }
-        });
+    chrome.tabs.onRemoved.addListener(function(tabId, info) {
+        delete requests[tabId];
+    });
 
-        chrome.tabs.onRemoved.addListener(function(tabId, info) {
-            delete requests[tabId];
-        });
-
-    }
 });
 
 chrome.storage.onChanged.addListener(function(changes, namespace) {
