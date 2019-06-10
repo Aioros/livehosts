29cacc840c633a0d4acef8bfcb17c1b832e97475
Aioros <aioros83@gmail.com>
2019-06-10 12:20:17 -0700

    New directory structure for cross-browser build chain

---
+++ tools/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+	then
+		echo "No argument supplied"
+		exit 1
+fi
+
+if [ -d ../dist/"$1" ]
+	then
+		rm -R ../dist/"$1"
+fi
+
+mkdir ../dist/"$1"
+cp -r ../src/* ../dist/"$1"/
+cp -r ../platform/"$1"/* ../dist/"$1"/
+cd ../dist/"$1"
+zip -qr livehosts.zip *
